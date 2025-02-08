f9b3d85faf84b1ed3c5487b1b79c4cc52f578f76
prashantmestry <mestry.prashant10@gmail.com>
2025-02-08 12:39:05 +0530

    theme adeed

--- app/__component/Header.tsx
+++ app/__component/Header.tsx
@@ -1,16 +1,30 @@
+"use client";
+import { useTheme } from "next-themes";
 import Link from "next/link";
 
 const Header = () => {
+  const { setTheme } = useTheme();
+
   return (
-    <div className="flex flex-row list-none p-2 bg-slate-300">
-      <li className="mr-2">
-        <Link href="/users">User Detail</Link>
-      </li>
-      <li className="mr-2">
-        <Link href="/">Home</Link>
-      </li>
+    <div className="flex flex-row justify-between list-none p-2 bg-slate-300">
+      <div className="flex">
+        <li className="mr-2">
+          <Link href="/users">User Detail</Link>
+        </li>
+        <li className="mr-2">
+          <Link href="/">Home</Link>
+        </li>
+        <div>
+          Environment : {process.env.NODE_ENV} = {process.env.API_DOMAIN}
+        </div>
+      </div>
       <div>
-        Environment : {process.env.NODE_ENV} = {process.env.API_DOMAIN}
+        <span className="mr-2 cursor-pointer " onClick={() => setTheme("dark")}>
+          Dark
+        </span>
+        <span className="mr-2 cursor-pointer" onClick={() => setTheme("light")}>
+          Light
+        </span>
       </div>
     </div>
   );
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import UserList from "./__component/UserList";
 import NewUserForm from "./__component/NewUserForm";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "User Detail",
+  description: "Create Update Delete User data",
+};
+
 const UserDetails = () => {
   return (
     <div className="flex justify-between ">
